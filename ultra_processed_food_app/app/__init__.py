2df320d05078f97909fc37a268aaa7f7ef224a7f
Noah Paige <noahlandonpaige@gmail.com>
2020-02-10 16:20:58 -0800

    Created a UI mock-up

--- ultra_processed_food_app/app/__init__.py
+++ ultra_processed_food_app/app/__init__.py
@@ -1,4 +1,25 @@
-import tkinter
-top = tkinter.Tk();
-# Code to add widgets will go here...
-top.mainloop()
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MainWindow(Screen):
+    pass
+
+class NovaScoreWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv_file = Builder.load_file("interface.kv")
+
+class UltraProcessedFoodAppApp(App):
+    def build(self):
+        return kv_file
+
+
+
+if __name__ == "__main__":
+    UltraProcessedFoodAppApp().run()
